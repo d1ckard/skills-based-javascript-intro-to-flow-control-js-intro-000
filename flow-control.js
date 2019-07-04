f78ca3ff16d93491a8ac812f65ee1f108486674c
RIchard Farrell <richardmfarrell@gmail.com>
2019-07-04 17:11:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,7 +30,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case "15":
+    case "'15'":
       return "You are a teenager"
     default :
       return "You have an age"
