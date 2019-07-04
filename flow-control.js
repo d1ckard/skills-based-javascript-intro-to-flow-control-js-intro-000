bfb52f55448746052ad40fff1210d43b52ac1ac1
RIchard Farrell <richardmfarrell@gmail.com>
2019-07-04 16:41:18 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,10 +16,10 @@ function ageChecker(age) {
   if (age >=13 && age <=19) {
     return ("You are a teenager!")
   }
-  else if (age <13) {
+  else if (age <=13) {
     return ("You are a kid")
   }
-  else if (age >19) {
+  else if (age >=20) {
     return ("You are a grownup")
   }
 }
